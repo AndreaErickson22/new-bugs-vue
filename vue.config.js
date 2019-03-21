25a68c2efe33beb5d33b61e50d18ca019c7dc500
Andrea <avertrees@yahoo.com>
2019-03-20 23:09:28 -0600

    11pmWed

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: 'github.com/AndreaErickson22/',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
